426c5830bee697c919c9bec60e7a347516711adb
fred.feng <fred.feng@thomascook.com.cn>
2021-01-20 11:26:50 +0800

    feat：将测试配置提升到外部

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['./src'],
+  roots: ['./packages'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   testPathIgnorePatterns: ['node_modules/'],
